c7792ae33615f276a7ebce68f5b3157f671c4ad4
alibekk93 <34392060+alibekk93@users.noreply.github.com>
2023-11-28 23:58:38 -0500

    Update idp_in_bacteria_functions.py

--- idp_in_bacteria_functions.py
+++ idp_in_bacteria_functions.py
@@ -41,55 +41,6 @@ def read_fasta(fasta_file: str) -> pd.DataFrame:
   return df
 
 
-### Read pickle file ###
-
-def read_pickle_file(file_name: str):
-  """A simple function to read pickle files
-
-  Parameters
-  ----------
-  file_name : srt
-    location of the pickle file
-
-  Returns
-  -------
-  any format, but best with pd.DataFrames or lists
-
-  """
-
-  print(file_name + ' loading...')
-  with open(file_name, 'rb') as f:
-    result = pickle.load(f)
-  print(file_name + ' loaded!')
-  return result
-
-
-### Binary disorder ###
-
-def binary_disorder(disorder_list: list, threshold: float=0.5) -> list:
-  """Returns a list of binary values for disordered
-  residues given a threshold
-
-  Parameters
-  ----------
-  disorder_list : list
-    a list of disorder values
-  threshold : float
-    a binary threshold, 0.5 by default. Disorder score
-    above the threshold means that the residue is
-    considered to be disordered
-
-  Returns
-  -------
-  list
-    a list of binary values indicating whether the
-    residue is disordered (1) or not (0)
-
-  """
-
-  return [1 if dis > threshold else 0 for dis in disorder_list]
-
-
 ### Find longest binary IDR ###
 
 def find_longest_binary_IDR(binary_disorder_list: list) -> int:
@@ -127,30 +78,6 @@ def find_longest_binary_IDR(binary_disorder_list: list) -> int:
       current_IDR_len = 0
 
   return max_IDR_len
-
-
-### Fix -infinitives ###
-
-def fix_neg_inf(df: pd.DataFrame, replacement=np.nan) -> pd.DataFrame:
-  """Replaces -inf values in a given dataframe
-
-  Parameters
-  ----------
-  df : pd.DataFrame
-    The dataframe with -inf to be replaced
-  replacement
-    The value to replace -inf with. Default
-    is np.nan
-
-  Returns:
-  --------
-  pd.DataFrame
-
-  """
-
-  df_new = df.copy()
-  df_new[df.astype(float) < 0] = replacement
-  return df_new
 
 
 ### Align sequences ###
@@ -272,135 +199,6 @@ def get_charge_hydrophobicity(seq: str) -> tuple:
   return mean_abs_charge, mean_hydrophobicity
 
 
-### Drop unavailable rows ###
-
-def drop_unavailable(proteome: pd.DataFrame, reset_index: bool=True) -> pd.DataFrame:
-  """Drop rows if not all models are available
-
-  """
-
-  mask = [proteome.disorder.iloc[n].shape[1]==4 for n in range(len(proteome))]
-  proteome = proteome[mask]
-  if reset_index == True:
-    proteome.reset_index(inplace=True, drop=True)
-  return proteome
-
-
-### Get additional columns with IDP data ###
-
-def get_additional_columns(proteome:pd.DataFrame) -> None:
-  """Add Additional Columns to Proteome DataFrame
-  This function calculates additional columns for the proteome DataFrame including:
-
-  disorder_combined: mean of all disorder models
-  disorder_binary: binary disorder values based on a threshold of 0.5
-  disorder_mean: mean disorder score for each protein
-  disorder_binary_mean: mean binary disorder value (fraction of disorder) for each protein
-  longest_IDR: length of the longest binary intrinsically disordered region for each protein
-  -----------
-  Parameters:
-  proteome (pd.DataFrame): The dataframe containing protein information.
-
-  Returns:
-  None
-  """
-
-  # get means of all models
-  proteome['disorder_combined'] = [np.mean(proteome.disorder.iloc[i], axis=1) for i in range(len(proteome))]
-  # get binary disorder values
-  proteome['disorder_binary'] = proteome.disorder_combined.apply(binary_disorder, threshold=0.5)
-  # get mean disorder values for each protein (mean disorder score)
-  proteome['disorder_mean'] = proteome.disorder_combined.apply(np.mean)
-  # get mean binary disorder values for each protein (fraction of disorder)
-  proteome['disorder_binary_mean'] = proteome.disorder_binary.apply(np.mean)
-  # find longest binary IDR for each protein
-  proteome['longest_IDR'] = proteome.disorder_binary.apply(find_longest_binary_IDR)
-  return
-
-
-### Get and process pairwise OMA file ###
-
-def get_OMA(species_tag:str, common_species_tag:str='THET8', oma_type:str='any') -> pd.DataFrame:
-  """Obtains pairwise OMA and returns a dataframe
-
-  -----------
-  Parameters:
-  species_tag (str): Tag for species to use in OMA.
-  common_species_tag (str): Tag for the second species to use in OMA. This should be the same
-  for all OMA files. 'THET8' by default.
-  oma_type (str): Type of OMA relationships to keep. 'any' by default. Other possibilities are
-  '1:1', 'n:1', 'm:1' and 'n:m'
-
-  Returns:
-  pd.DataFrame
-  """
-
-  # get file
-  path = f'https://omabrowser.org/cgi-bin/gateway.pl?f=PairwiseOrthologs&p1={common_species_tag}&p2={species_tag}&p3=UniProt'
-  try_retrieve_url(path, '_')
-  OMA = pd.read_csv('_', sep='\t')
-  if len(OMA) == 0:
-    path = f'https://omabrowser.org/cgi-bin/gateway.pl?f=PairwiseOrthologs&p1={species_tag}&p2={common_species_tag}&p3=UniProt'
-    try_retrieve_url(path, '_')
-    OMA = pd.read_csv('_', sep='\t')
-  # process file
-  OMA.columns = [common_species_tag, species_tag, 'oma_type', 'OMA_group']
-  if oma_type != 'any':
-    OMA = OMA[OMA['oma_type'] == oma_type]
-  OMA.drop(['OMA_group', 'oma_type'], axis='columns', inplace=True)
-  OMA[common_species_tag] = OMA[common_species_tag].str.replace(common_species_tag, '')
-  OMA[common_species_tag] = OMA[common_species_tag].str.replace('_', '')
-  # OMA = OMA[OMA[common_species_tag].str.len()==6]
-  OMA[species_tag] = OMA[species_tag].str.replace(species_tag, '')
-  OMA[species_tag] = OMA[species_tag].str.replace('_', '')
-  OMA.reset_index(inplace=True, drop=True)
-  return OMA
-
-
-### Read fIDPnn disorder files ###
-
-def read_fIDPnn_disorder(path, insert_into_df=False, original_df=None):
-  """Processes sequence data from a file and returns DataFrames or updates an existing DataFrame
-
-    ----------
-    Parameters:
-    path (str) : The path to the input file containing sequence data.
-    insert_into_df (bool, optional) : Whether to insert processed data into an existing DataFrame. Default is False.
-    original_df (pd.DataFrame, optionsl) : Original DataFrame to insert into if insert_into_df=True. Default is None.
-
-    Returns:
-    None if insert_into_df is True, otherwise a dictionary of DataFrames.
-    
-  """
-  
-  dfs = {}
-  lists = {}
-  dflines = []
-
-  with open(path, 'rb') as f:
-    for i, line in enumerate(tqdm(f.readlines())):
-      if line.decode('utf-8')[:8] == 'Sequence':
-        continue
-      elif line.decode('utf-8')[0] == '>':
-        if dflines != []:
-          columns = dflines[0].lower().replace(' ', '_').strip().split(',')
-          df = pd.DataFrame([l.strip().split(',') for l in dflines[1:]], columns=columns)
-          df.iloc[:, 2:] = df.iloc[:, 2:].astype('float')
-          df[df.columns[0]] = df.iloc[:, 0].astype('float')
-          dfs[name] = df
-          lists[name] = df.predicted_score_for_disorder.tolist()
-        name = line.decode('utf-8').split('|')[1]
-        dflines = []
-      else:
-        dflines.append(line.decode('utf-8'))
-  if insert_into_df:
-    m = original_df.disorder_fIDPnn.isna()
-    original_df.loc[m, 'disorder_fIDPnn'] = original_df.ID.map(lists)
-    return
-  else:
-    return dfs
-
-
 ### Plot aligned cluster ###
 
 def plot_aligned_cluster(df:pd.DataFrame, cluster:str, mav:int=50, errorbar=('ci', 95), scatter:bool=False) -> None:
@@ -459,3 +257,210 @@ def plot_aligned_cluster(df:pd.DataFrame, cluster:str, mav:int=50, errorbar=('ci
   ax.legend()
 
   fig.show()
+
+
+###################################
+### OLD FUNCTIONS - no longer used
+
+# ### Read pickle file ###
+
+# def read_pickle_file(file_name: str):
+#   """A simple function to read pickle files
+
+#   Parameters
+#   ----------
+#   file_name : srt
+#     location of the pickle file
+
+#   Returns
+#   -------
+#   any format, but best with pd.DataFrames or lists
+
+#   """
+
+#   print(file_name + ' loading...')
+#   with open(file_name, 'rb') as f:
+#     result = pickle.load(f)
+#   print(file_name + ' loaded!')
+#   return result
+
+
+# ### Binary disorder ###
+
+# def binary_disorder(disorder_list: list, threshold: float=0.5) -> list:
+#   """Returns a list of binary values for disordered
+#   residues given a threshold
+
+#   Parameters
+#   ----------
+#   disorder_list : list
+#     a list of disorder values
+#   threshold : float
+#     a binary threshold, 0.5 by default. Disorder score
+#     above the threshold means that the residue is
+#     considered to be disordered
+
+#   Returns
+#   -------
+#   list
+#     a list of binary values indicating whether the
+#     residue is disordered (1) or not (0)
+
+#   """
+
+#   return [1 if dis > threshold else 0 for dis in disorder_list]
+
+
+# ### Fix -infinitives ###
+
+# def fix_neg_inf(df: pd.DataFrame, replacement=np.nan) -> pd.DataFrame:
+#   """Replaces -inf values in a given dataframe
+
+#   Parameters
+#   ----------
+#   df : pd.DataFrame
+#     The dataframe with -inf to be replaced
+#   replacement
+#     The value to replace -inf with. Default
+#     is np.nan
+
+#   Returns:
+#   --------
+#   pd.DataFrame
+
+#   """
+
+#   df_new = df.copy()
+#   df_new[df.astype(float) < 0] = replacement
+#   return df_new
+
+
+# ### Drop unavailable rows ###
+
+# def drop_unavailable(proteome: pd.DataFrame, reset_index: bool=True) -> pd.DataFrame:
+#   """Drop rows if not all models are available
+
+#   """
+
+#   mask = [proteome.disorder.iloc[n].shape[1]==4 for n in range(len(proteome))]
+#   proteome = proteome[mask]
+#   if reset_index == True:
+#     proteome.reset_index(inplace=True, drop=True)
+#   return proteome
+
+
+# ### Get additional columns with IDP data ###
+
+# def get_additional_columns(proteome:pd.DataFrame) -> None:
+#   """Add Additional Columns to Proteome DataFrame
+#   This function calculates additional columns for the proteome DataFrame including:
+
+#   disorder_combined: mean of all disorder models
+#   disorder_binary: binary disorder values based on a threshold of 0.5
+#   disorder_mean: mean disorder score for each protein
+#   disorder_binary_mean: mean binary disorder value (fraction of disorder) for each protein
+#   longest_IDR: length of the longest binary intrinsically disordered region for each protein
+#   -----------
+#   Parameters:
+#   proteome (pd.DataFrame): The dataframe containing protein information.
+
+#   Returns:
+#   None
+#   """
+
+#   # get means of all models
+#   proteome['disorder_combined'] = [np.mean(proteome.disorder.iloc[i], axis=1) for i in range(len(proteome))]
+#   # get binary disorder values
+#   proteome['disorder_binary'] = proteome.disorder_combined.apply(binary_disorder, threshold=0.5)
+#   # get mean disorder values for each protein (mean disorder score)
+#   proteome['disorder_mean'] = proteome.disorder_combined.apply(np.mean)
+#   # get mean binary disorder values for each protein (fraction of disorder)
+#   proteome['disorder_binary_mean'] = proteome.disorder_binary.apply(np.mean)
+#   # find longest binary IDR for each protein
+#   proteome['longest_IDR'] = proteome.disorder_binary.apply(find_longest_binary_IDR)
+#   return
+
+
+# ### Get and process pairwise OMA file ###
+
+# def get_OMA(species_tag:str, common_species_tag:str='THET8', oma_type:str='any') -> pd.DataFrame:
+#   """Obtains pairwise OMA and returns a dataframe
+
+#   -----------
+#   Parameters:
+#   species_tag (str): Tag for species to use in OMA.
+#   common_species_tag (str): Tag for the second species to use in OMA. This should be the same
+#   for all OMA files. 'THET8' by default.
+#   oma_type (str): Type of OMA relationships to keep. 'any' by default. Other possibilities are
+#   '1:1', 'n:1', 'm:1' and 'n:m'
+
+#   Returns:
+#   pd.DataFrame
+#   """
+
+#   # get file
+#   path = f'https://omabrowser.org/cgi-bin/gateway.pl?f=PairwiseOrthologs&p1={common_species_tag}&p2={species_tag}&p3=UniProt'
+#   try_retrieve_url(path, '_')
+#   OMA = pd.read_csv('_', sep='\t')
+#   if len(OMA) == 0:
+#     path = f'https://omabrowser.org/cgi-bin/gateway.pl?f=PairwiseOrthologs&p1={species_tag}&p2={common_species_tag}&p3=UniProt'
+#     try_retrieve_url(path, '_')
+#     OMA = pd.read_csv('_', sep='\t')
+#   # process file
+#   OMA.columns = [common_species_tag, species_tag, 'oma_type', 'OMA_group']
+#   if oma_type != 'any':
+#     OMA = OMA[OMA['oma_type'] == oma_type]
+#   OMA.drop(['OMA_group', 'oma_type'], axis='columns', inplace=True)
+#   OMA[common_species_tag] = OMA[common_species_tag].str.replace(common_species_tag, '')
+#   OMA[common_species_tag] = OMA[common_species_tag].str.replace('_', '')
+#   # OMA = OMA[OMA[common_species_tag].str.len()==6]
+#   OMA[species_tag] = OMA[species_tag].str.replace(species_tag, '')
+#   OMA[species_tag] = OMA[species_tag].str.replace('_', '')
+#   OMA.reset_index(inplace=True, drop=True)
+#   return OMA
+
+
+# ### Read fIDPnn disorder files ###
+
+# def read_fIDPnn_disorder(path, insert_into_df=False, original_df=None):
+#   """Processes sequence data from a file and returns DataFrames or updates an existing DataFrame
+
+#     ----------
+#     Parameters:
+#     path (str) : The path to the input file containing sequence data.
+#     insert_into_df (bool, optional) : Whether to insert processed data into an existing DataFrame. Default is False.
+#     original_df (pd.DataFrame, optionsl) : Original DataFrame to insert into if insert_into_df=True. Default is None.
+
+#     Returns:
+#     None if insert_into_df is True, otherwise a dictionary of DataFrames.
+    
+#   """
+  
+#   dfs = {}
+#   lists = {}
+#   dflines = []
+
+#   with open(path, 'rb') as f:
+#     for i, line in enumerate(tqdm(f.readlines())):
+#       if line.decode('utf-8')[:8] == 'Sequence':
+#         continue
+#       elif line.decode('utf-8')[0] == '>':
+#         if dflines != []:
+#           columns = dflines[0].lower().replace(' ', '_').strip().split(',')
+#           df = pd.DataFrame([l.strip().split(',') for l in dflines[1:]], columns=columns)
+#           df.iloc[:, 2:] = df.iloc[:, 2:].astype('float')
+#           df[df.columns[0]] = df.iloc[:, 0].astype('float')
+#           dfs[name] = df
+#           lists[name] = df.predicted_score_for_disorder.tolist()
+#         name = line.decode('utf-8').split('|')[1]
+#         dflines = []
+#       else:
+#         dflines.append(line.decode('utf-8'))
+#   if insert_into_df:
+#     m = original_df.disorder_fIDPnn.isna()
+#     original_df.loc[m, 'disorder_fIDPnn'] = original_df.ID.map(lists)
+#     return
+#   else:
+#     return dfs
+
+
